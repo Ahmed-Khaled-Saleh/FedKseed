f3c980a365b412cbe789cae63a1d5667966cc661
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2024-07-27 23:24:20 +0300

    adjusted .gitignore file

--- servers/server_fedu.py
+++ servers/server_fedu.py
@@ -1,26 +1,30 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from peft import LoraConfig, get_peft_model
-import torch
-from tqdm import tqdm
-from utils.validation import *
-from utils_data.default_tokens import DefaultToken
-from copy import deepcopy
+import numpy as np
+import pandas as pd
 import os
 import math
-from optimizers.mezo_optimizer import *
+from copy import deepcopy
 
-
-
+from tqdm import tqdm
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from peft import LoraConfig, get_peft_model
+import wandb
+from data.utils_data.default_tokens import DefaultToken
+from optimizers.mezo_optimizer import *  # noqa: F403
+from utils.validation import *  # noqa: F403
+from utils.helper_fuctions import *  # noqa: F403
+from trainers.trainer import Trainer
+from trainers.callbacks import empty_cach, log_memory
 
 class Server(object):
-    def __init__(self, args, eval_loader, candidate_seeds, log_dir):
+    def __init__(self, args, candidate_seeds, log_dir):
         self.args = args
-        self.eval_loader = eval_loader
         self.candidate_seeds = candidate_seeds
         self.tokenizer = AutoTokenizer.from_pretrained(args.model, use_fast=True)
         self.log_dir = log_dir
         self.tokenizer.model_max_length = self.args.max_length
         special_tokens = dict()
+ 
         if self.tokenizer.pad_token is None:
             special_tokens["pad_token"] = DefaultToken.PAD_TOKEN.value
         if self.tokenizer.eos_token is None:
@@ -31,18 +35,16 @@ class Server(object):
             special_tokens["unk_token"] = DefaultToken.UNK_TOKEN.value
         self.tokenizer.add_special_tokens(special_tokens)
         
-        # self.model = AutoModelForCausalLM.from_pretrained(args.model, device_map='cpu', torch_dtype=torch.float16, trust_remote_code=True)
+        self.model = AutoModelForCausalLM.from_pretrained(args.model, device_map='cpu', torch_dtype=torch.float16, trust_remote_code=True)
 
-        self.base_model = AutoModelForCausalLM.from_pretrained(args.model)
-        from copy import deepcopy
-        config = LoraConfig(
-            r=self.args.r,
-            lora_alpha=16,
-            lora_dropout=0.1,
-            bias="none")
-        self.model = get_peft_model(deepcopy(self.base_model), config)
-
-
+        # self.base_model = AutoModelForCausalLM.from_pretrained(args.model)
+        
+        # config = LoraConfig(
+        #     r=self.args.r,
+        #     lora_alpha=16,
+        #     lora_dropout=0.1,
+        #     bias="none")
+        # self.model = self.base_model#get_peft_model(deepcopy(self.base_model), config)
 
         self.model_w0 = deepcopy(self.model)
         self.seed_pool = {seed: 0.0 for seed in self.candidate_seeds}
@@ -57,15 +59,63 @@ class Server(object):
             self.gradient_history = None
             self.probabilities = None
     
+
+    def train(self,
+              client_list,
+              client_indices_rounds,
+              args,
+              run,
+              memory_record_dic):
+
+        lst_global_metrics_dfs = []
+        for t in range(1, args.rounds + 1):
+            selected_client = [client_list[i] for i in client_indices_rounds[t-1]]
+            
+            lst_global_metrics = []
+            
+            for client in selected_client:
+                trainer = Trainer(client)
+            
+                local_iters = client.args.local_step
+                epochs = 1
+                
+                client.model = deepcopy(self.model)
+                
+                metrics = {}
+                train_loss, val_loss, train_acc, val_acc = trainer.train(fed= True,
+                                                                         epochs= epochs,
+                                                                         local_iters= local_iters,
+                                                                         memory_record_dic= memory_record_dic,
+                                                                         callbacks=[empty_cach, log_memory])
+                
+                train_loss = np.array(train_loss).mean()
+                task = client.task if isinstance(client.task, str) else client.task[0]
+
+                metrics['train_loss'], metrics['val_loss'], metrics['task'], metrics['train_acc'], metrics['val_acc'] = \
+                    train_loss, val_loss, task, train_acc, val_acc
+                
+                lst_global_metrics.append(metrics)
+            
+            round_global_metrics = wandb.Table(dataframe=pd.DataFrame(lst_global_metrics))
+            run.log({f"round {t} (GM) Metrics": round_global_metrics})
+            
+            lst_global_metrics_dfs.append(pd.DataFrame(lst_global_metrics))
+
+            self.aggregate_seed_pool(selected_client)
+            self.update_global_model_by_seed_pool()
+
+        return lst_global_metrics_dfs
+
     def create_model_by_seedpool(self, cur_round):
         tmp_model = deepcopy(self.model_w0)
         tmp_model.to(self.device)
-        
-        lr = self.args.lr * math.pow(self.args.lr_decay, cur_round - 1)
+
+        lr = float(self.args.lr)
+        lr = lr * math.pow(self.args.lr_decay, cur_round - 1)
         if self.args.lr_decay != 1.0:
             raise ValueError('currently seed pool only supports constant learning rate')
         # replace local model with initial weights
-        framework = MeZOFramework(tmp_model, args=self.args, lr=lr, candidate_seeds=self.candidate_seeds)
+        framework = MeZOFramework(tmp_model, args=self.args, lr=lr, candidate_seeds=self.candidate_seeds)  # noqa: F405
         progress_bar = tqdm(range(len(self.seed_pool))) 
         # pull the latest model via accumulated {seed, grad} pairs on the server
         for seed, grad in self.seed_pool.items():
@@ -77,16 +127,19 @@ class Server(object):
         return tmp_model
 
     def aggregate_seed_pool(self, selected_client_list):
+        # step 7 in the FedK algorithm
         if self.args.equal_weight:
             weight_array = np.array([1.0 for _ in selected_client_list], dtype=np.float64)
             weight_array /= float(len(selected_client_list))
         else:
             weight_array = np.array([len(client.train_loader) for client in selected_client_list], dtype=np.float64)
             weight_array /= float(np.sum(weight_array))
+        
         for client_idx in range(len(selected_client_list)):
             local_seed_pool = selected_client_list[client_idx].local_seed_pool
             for seed, grad in local_seed_pool.items():
                 self.seed_pool[seed] += grad * weight_array[client_idx]
+
         for client in selected_client_list:
             client.clear_model()
 
@@ -94,7 +147,7 @@ class Server(object):
         self.model = deepcopy(self.model_w0)
         self.model.to(self.device)
         
-        framework = MeZOFramework(self.model, args=self.args, lr=self.args.lr, candidate_seeds=self.candidate_seeds)
+        framework = MeZOFramework(self.model, args=self.args, lr=float(self.args.lr), candidate_seeds=self.candidate_seeds)  # noqa: F405
         progress_bar = tqdm(range(len(self.seed_pool))) 
 
         # pull the latest model via accumulated {seed, grad} pairs on the server
@@ -102,7 +155,7 @@ class Server(object):
             if grad != 0.0:
                 framework.zo_update(seed=seed, grad=grad)
             progress_bar.update(1)
-            progress_bar.set_description(f'server update global model')
+            progress_bar.set_description('server update global model')
 
     def prepare_aggregate(self):
         self.model_for_aggregate = deepcopy(self.model)
@@ -136,12 +189,36 @@ class Server(object):
     def calculate_probabilities(self):
         history_list = [self.gradient_history[seed] for seed in self.candidate_seeds]
         mean_grad_history = np.array([np.mean(np.abs(np.clip(history_cur_seed, -self.args.bias_loss_clip, self.args.bias_loss_clip))) for history_cur_seed in history_list])
-        self.probabilities = softmax(min_max_norm(mean_grad_history))
+        self.probabilities = softmax(min_max_norm(mean_grad_history))  # noqa: F405
         sum_prob = np.sum(self.probabilities)
         if sum_prob != 1.0:
             self.probabilities /= sum_prob
         return self.probabilities
 
+
+    def eval_clients(self, clients_list ,cur_round, include_eval= False):
+        clients_metrics = []
+
+        for client in clients_list:
+            metrics = {}
+
+            task = client.task
+            metrics['task'] = task
+
+            train_acc, train_loss = client.train_error_and_loss(deepcopy(self.model))
+            metrics['train_loss'] = train_loss
+            metrics['train_acc'] = train_acc
+
+            if include_eval:
+                eval_acc, eval_loss = client.eval_error_and_loss()             
+                metrics['eval_loss'] = eval_loss
+                metrics['eval_acc'] = eval_acc
+
+            clients_metrics.append(metrics)
+        
+        return clients_metrics
+    
+    
     def eval(self, cur_round, eval_avg_acc):
         if self.args.eval_metric == 'loss':
             eval_metric = self.eval_loss(cur_round)
@@ -163,6 +240,7 @@ class Server(object):
             torch.save(self.model.state_dict(), os.path.join(save_dir, f'model_state_dict_final_round{cur_round}.bin'))
         return eval_metric
 
+    
     def eval_loss(self, cur_round):
         self.model = self.model.to(self.device)
         self.model.eval()
@@ -171,15 +249,18 @@ class Server(object):
         loss_total_eval = 0.0
         num_eval = 0
         
-        with torch.inference_mode():
+        loss_per_task = {}
+        with torch.no_grad():
             for batch in self.eval_loader:
+                task = batch['task'][0]
                 batch = {
                     'input_ids': batch['input_ids'].to(self.device),
                     'labels': batch['labels'].to(self.device),
-                    'attention_mask': batch['attention_mask'].to(self.device) 
+                    'attention_mask': batch['attention_mask'].to(self.device)
                 }
                 outputs = self.model(**batch)
                 loss = outputs.loss
+                loss_per_task[task] = loss if task not in loss_per_task else loss_per_task[task] + loss
                 progress_bar_eval.update(1)
                 if torch.isnan(loss):
                     continue
@@ -191,7 +272,7 @@ class Server(object):
         print()
         print()
         self.model = self.model.cpu()
-        return (loss_total_eval / num_eval).item()
+        return (loss_total_eval / num_eval).item(), loss_per_task
 
     def eval_generate(self, cur_round):
         self.model = self.model.to(self.device)
@@ -201,7 +282,7 @@ class Server(object):
         acc_total_eval = 0.0
         num_eval = 0
         
-        with torch.inference_mode():
+        with torch.no_grad():
             for batch in self.eval_loader:
                 input_ids = batch['input_ids'].to(self.device)
                 label_ids = batch['labels'].to(self.device)
@@ -210,7 +291,7 @@ class Server(object):
                     max_new_tokens=128,
                     num_beams=1,
                 )
-                acc_total_eval += rouge_score(output_ids[0][len(input_ids[0]):], label_ids[0], self.tokenizer)
+                acc_total_eval += rouge_score(output_ids[0][len(input_ids[0]):], label_ids[0], self.tokenizer)  # noqa: F405
                 progress_bar_eval.update(1)
                 num_eval += len(batch['input_ids'])
                 if num_eval == 0:
