21599befd5f1e2992e59ba2b4727223660f3c01b
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2024-07-28 18:43:24 +0300

    corrected jobs files

--- jobs/fedu.sh
+++ jobs/fedu.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-#SBATCH --job-name=fedkseed
 #SBATCH --account=project_2009050
-#SBATCH --partition=gpu
-#SBATCH --time=15:00:00
+#SBATCH --job-name=fedkseed
+#SBATCH --partition=gpusmall
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
-#SBATCH --mem-per-cpu=8000
-#SBATCH --gres=gpu:v100:1,nvme:10
+#SBATCH --cpus-per-task=40
+#SBATCH --mem=320G
+#SBATCH --time=15:00:00
+#SBATCH --gres=gpu:a100:1
 #SBATCH --output=logs/fedkseed.out
 #SBATCH --error=logs/fedkseed.err
-#SBATCH --mail-type=BEGIN
 
-
+module --force purge
+module load pytorch
+source /projappl/project_2009050/torch/bin/activate
 cd /projappl/project_2009050/code/FedKseed
-source /projappl/project_2009050/python_envs/bin/activate
-srun python main.py --fname ./configs/fedk/configs.yaml
+
+export PYTHONPATH=$PYTHONPATH:/projappl/project_2009050/torch/lib/python3.9/site-packages
+echo "Current PYTHONPATH: $PYTHONPATH"
+python -c "import rouge; print('rouge module is installed and importable')"
+
+srun python main.py --fname ./configs/fedu/configs.yaml
