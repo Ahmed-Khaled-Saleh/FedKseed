20ad2e68e9ebf4b1e60b9180449a8361450cf4c6
Ahmed-Khaled-Saleh <ahmedkhaledsaleh11@gmail.com>
2024-07-30 20:14:57 +0300

    syync

--- main.py
+++ main.py
@@ -61,7 +61,9 @@ def process_main(args_config_fname):
 
     args = argparse.Namespace(**experiment)
     run = wandb.init(project=args.project, name= args.name, config=args)
-
+    client_indices_rounds = get_client_indices_rounds(args)
+    print("client indices rounds generated", client_indices_rounds)
+    
     time_stamp = str(time.time())
     memory_record_dic = {}
     
@@ -94,8 +96,7 @@ def process_main(args_config_fname):
 
     # since only CUDA device is available, load all models on device 0
     args.device = 0
-    client_indices_rounds = get_client_indices_rounds(args)
-    print("client indices rounds generated", client_indices_rounds)
+    
     # sample `K` candidate seeds
     candidate_seeds = np.random.randint(1, 100000000000, args.K)
     
